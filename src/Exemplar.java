e695fa1dd5aef7c8c45f59f1d5d4b06b87c30818
kael <kaelscott8@gmail.com>
2024-05-30 16:25:49 -0300

    add Exemplar.java

---
+++ src/Exemplar.java
@@ -0,0 +1,44 @@
+import java.time.LocalDate;
+
+public abstract class Exemplar {
+    protected int classind;
+    protected String titulo;
+    protected LocalDate devolucao = null;
+    protected boolean status = false;
+    public Exemplar(int classind, String titulo) {
+        this.classind = classind;
+        this.titulo = titulo;
+    }
+    public int getClassind() {
+        return classind;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public LocalDate getDevolucao() {
+        return devolucao;
+    }
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setClassind(int classind) {
+        this.classind = classind;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    public abstract void extenderDevolucao();
+    public void emprestar() {
+        status = true;
+    }
+    public void devolver() {
+        status = false;
+        devolucao = null;
+    }
+}
+
+// Path: src/Livro.java
