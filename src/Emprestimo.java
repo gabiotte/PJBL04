d70394747775c82c6f21201ac734504f8b77e31c
ger777 <112423104+ger777@users.noreply.github.com>
2024-06-07 08:26:23 -0300

    Add files via upload

--- src/Emprestimo.java
+++ src/Emprestimo.java
@@ -1,15 +1,16 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.time.temporal.ChronoUnit;
+
 
 public class Emprestimo {
     private Cliente cliente;
     private ArrayList<Exemplar> exemplares;
-    private LocalDate data;
+    private LocalDate data = LocalDate.now();
     private float multa = 0;
-    public Emprestimo(Cliente cliente, ArrayList<Exemplar> exemplares, LocalDate data) {
+    public Emprestimo(Cliente cliente, ArrayList<Exemplar> exemplares) {
         this.cliente = cliente;
         this.exemplares = exemplares;
-        this.data = data;
     }
     public Cliente getCliente() {
         return cliente;
@@ -26,4 +27,17 @@ public class Emprestimo {
     public void setMulta(float multa) {
         this.multa = multa;
     }
+
+    public void calcularMulta() {
+        for (Exemplar exemplar:exemplares) {
+            if (exemplar.devolucao != null) {
+                if (exemplar.devolucao.isBefore(LocalDate.now())) {
+                    multa = ChronoUnit.DAYS.between(LocalDate.now(), exemplar.devolucao) * 2;
+                    continue;
+                }
+            }
+            multa = 0;
+        }
+    }
+
 }
